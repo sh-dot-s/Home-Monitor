b860ba89100949dda7fc8c942a9abfbd217f3c60
sree1952425 <sree1952425@gmail.com>
2016-11-19 01:38:33 +0530

    Commit 3 Major #1

--- app/src/main/java/com/raspberrypi/slash/simple_db/MainActivity.java
+++ app/src/main/java/com/raspberrypi/slash/simple_db/MainActivity.java
@@ -1,5 +1,6 @@
 package com.raspberrypi.slash.simple_db;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,23 +11,56 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    EditText email = (EditText) findViewById(R.id.editText3);
-    Button register = (Button) findViewById(R.id.button_2);
-    TextView token = (TextView) findViewById(R.id.textView);
+    Button register;
+    TextView token;
+    EditText email;
+    public View.OnClickListener getEmail;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                token.setText(Constants.SHARED_PREF);
-            }
-        });*/
+        register = (Button) findViewById(R.id.button_reg);
+        token = (TextView) findViewById(R.id.textView);
+        email = (EditText) findViewById(R.id.editText3);
+        register.setOnClickListener(getEmail);
+    }
+    public void sendRegistrationToServer(String token){
+        final String registrationToken = Constants.FCM_TOKEN;
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(Constants.REGISTER_URL);
+
+        try {
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("firebaseid", registrationToken));
+            nameValuePairs.add(new BasicNameValuePair("email","slash" ));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
     }
 
     @Override
--- app/src/main/java/com/raspberrypi/slash/simple_db/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/raspberrypi/slash/simple_db/MyFirebaseInstanceIDService.java
@@ -3,26 +3,21 @@ package com.raspberrypi.slash.simple_db;
 import android.content.SharedPreferences;
 import android.util.Log;
 
+
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
 public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
     private static final String TAG = "MyFirebaseIIDService";
-
     @Override
     public void onTokenRefresh() {
         // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-
         Log.d(TAG, "Refreshed token: " + refreshedToken);
-
-        //TODO: Implement this method to send any registration to your app's servers.
-        sendRegistrationToServer(refreshedToken);
-    }
-    public void sendRegistrationToServer(String token){
-        SharedPreferences sharedPreferences = getSharedPreferences(Constants.SHARED_PREF, MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(Constants.SHARED_PREF, token);
+        SharedPreferences sharedPreference = getSharedPreferences(Constants.FCM_TOKEN,MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreference.edit();
+        editor.putString(Constants.FCM_TOKEN,refreshedToken);
         editor.commit();
+        //TODO: Implement this method to send any registration to your app's servers.
     }
 }
